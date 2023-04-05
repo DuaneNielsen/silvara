bef0dababbac22de73284d4a0860d4474d00c78d
duane <duane.nielsen.rocks@gmail.com>
2023-04-05 10:19:24 -0700

    histograms and area

--- script/silvara.py
+++ script/silvara.py
@@ -23,13 +23,15 @@ import traceback
 from collections import deque
 import numpy as np
 import cv2
+import torch
+import torch.nn as nn
 
 # ROS
 import rospy
 
 from std_msgs.msg import Int16
 # MAVROS
-from mavros_msgs.msg import OverrideRCIn
+from mavros_msgs.msg import OverrideRCIn, RCIn, RCOut
 # from mavros_msgs.srv import CommandBool
 from mavros_msgs.msg import WaypointList, WaypointReached
 
@@ -65,6 +67,14 @@ def get_servo_pwm(pos):
     return pwm
 
 
+def get_throttle_pwm(pos):
+    """ takes a number in range -1.0 .. 1.0 and converts to the servo pulse width"""
+    pwm = int(rospy.get_param('/THROTTLE_RANGE') * pos + rospy.get_param('/THROTTLE_CENTER') + rospy.get_param('/THROTTLE_TRIM'))
+    pwm = min(rospy.get_param('/THROTTLE_MAX'), pwm)
+    pwm = max(rospy.get_param('/THROTTLE_MIN'), pwm)
+    return pwm
+
+
 class UAV_Control:
     """UAV WP and Manual Control"""
 
@@ -97,17 +107,15 @@ class UAV_Control:
             '/mavros/cmd/command', CommandLong, persistent=True)
 
         # Publishers
-        self.pub_rc_override = rospy.Publisher(
-            'mavros/rc/override', OverrideRCIn, queue_size=10)
+        self.pub_rc_override = rospy.Publisher('mavros/rc/override', OverrideRCIn, queue_size=10)
 
         # Subscribers
-        self.sub_waypoints = rospy.Subscriber(
-            "/mavros/mission/waypoints",
-            WaypointList, self.__waypoints_cb)
-
-        self.sub_current = rospy.Subscriber(
-            "/mavros/mission/reached", WaypointReached,
-            self.__current_cb)
+        self.sub_waypoints = rospy.Subscriber("/mavros/mission/waypoints", WaypointList, self.__waypoints_cb)
+        self.sub_current = rospy.Subscriber("/mavros/mission/reached", WaypointReached, self.__current_cb)
+        self.sub_rc_in = rospy.Subscriber("/mavros/rc/in", RCIn, self.__rcin_cb)
+        self.sub_rc_out = rospy.Subscriber("/mavros/rc/out", RCOut, self.__rcout_cb)
+        self.servo = 1500
+        self.throttle= 1000
 
     def __waypoints_cb(self, topic):
         self.lock.acquire()
@@ -138,6 +146,15 @@ class UAV_Control:
         finally:
             self.lock.release()
 
+    def __rcin_cb(self, rc_in):
+        throttle = rc_in.channels[THROTTLE_CHANNEL]
+        self.set_throttle_servo(self.throttle, self.servo)
+        # rospy.loginfo(rc_in)
+
+    def __rcout_cb(self, rc_out):
+        pass
+        #rospy.loginfo(rc_out)
+
     def print_waypoints(self):
         """Prints Pixhawk waypoints to stdout"""
         for seq, waypoint in enumerate(self.waypoint_list):
@@ -163,7 +180,7 @@ class UAV_Control:
         # rospy.loginfo('mavros/rc/override, throttle')
         msg = OverrideRCIn()
         msg.channels[THROTTLE_CHANNEL] = throttle  # Desired PWM value
-        rospy.loginfo(msg)
+        # rospy.loginfo(msg)
         self.pub_rc_override.publish(msg)
 
     #
@@ -174,7 +191,7 @@ class UAV_Control:
         # rospy.loginfo('mavros/rc/override, servo')
         msg = OverrideRCIn()
         msg.channels[STEER_CHANNEL] = servo  # Desired PWM value
-        rospy.loginfo(msg)
+        # rospy.loginfo(msg)
         self.pub_rc_override.publish(msg)
 
     #
@@ -185,8 +202,9 @@ class UAV_Control:
         """Set throttle AND servo"""
         # rospy.loginfo('mavros/rc/override, throttle and servo')
         msg = OverrideRCIn()
-        msg.channels[THROTTLE_CHANNEL] = throttle  # Desired PWM value
-        msg.channels[STEER_CHANNEL] = servo  # Desired PWM value
+        if current_state.mode == "MANUAL":
+            msg.channels[THROTTLE_CHANNEL] = throttle  # Desired PWM value
+            msg.channels[STEER_CHANNEL] = servo  # Desired PWM value
         rospy.loginfo(msg)
         self.pub_rc_override.publish(msg)
 
@@ -293,10 +311,10 @@ class UAV_Control:
 
     def send_mavros_cmd(self, bool1, msgid, bool2, p0, p1, p2, p3, p4, p5, p6):
         """Send a mavros command"""
-        rospy.loginfo("/mavros/cmd/command/ %s %s %s %s %s %s %s %s %s %s",
-                      str(bool1), str(msgid),
-                      str(bool2), str(p0),
-                      str(p1), str(p2), str(p3), str(p4), str(p5), str(p6))
+        # rospy.loginfo("/mavros/cmd/command/ %s %s %s %s %s %s %s %s %s %s",
+        #              str(bool1), str(msgid),
+        #              str(bool2), str(p0),
+        #              str(p1), str(p2), str(p3), str(p4), str(p5), str(p6))
         self._srv_cmd_long(bool1, msgid, bool2, p0, p1, p2, p3, p4, p5, p6)
 
 
@@ -319,9 +337,16 @@ def restore_stdout(original_stdout):
     # Reset stdout to the original value
     sys.stdout = original_stdout
 
+class LogInfoMessage:
+    def __init__(self, cooldown=0.0):
+        self.cooldown = rospy.Duration(cooldown)
+        self.last_logged = rospy.Time.now() - self.cooldown
 
-global best_track
-best_track = None
+    def __call__(self, log_mesg):
+        if rospy.Time.now() - self.last_logged > self.cooldown:
+            rospy.loginfo(log_mesg)
+            self.last_logged = rospy.Time.now()
+
 
 if __name__ == "__main__":
 
@@ -349,9 +374,26 @@ if __name__ == "__main__":
 
     # PID queue
     angle_to_object = deque(maxlen=rospy.get_param('SERVO_PID_LEN'))
+    dist_to_object = deque(maxlen=rospy.get_param('THROTTLE_PID_LEN'))
 
     tracking_map = {0: "OFF", 1: "OK", 2: "SEARCHING", 3: "TERMINATE", 4:"LAST"}
 
+    in_features = 64 * 3
+    mlp_size = 4
+
+    net = nn.Sequential(
+        nn.Linear(in_features, mlp_size),
+        nn.BatchNorm1d(mlp_size),
+        nn.ReLU(inplace=True),
+        nn.Linear(mlp_size, mlp_size),
+        nn.BatchNorm1d(mlp_size),
+        nn.ReLU(inplace=True),
+        nn.Linear(mlp_size, 1, bias=False),
+        nn.Sigmoid()
+    )
+
+    net.load_state_dict(torch.load('/home/duane/color_mlp_weights.pt'))
+    net.eval()
 
     def format_track(track):
         return f"id: {track.label_id} conf: {track.confidence:.2f} " \
@@ -359,28 +401,62 @@ if __name__ == "__main__":
                f"pos: {track.position[0]:.2f}, {track.position[1]:.2f}, {track.position[2]:.2f}" \
                f" vel:{track.velocity[0]:.2f}, {track.velocity[1]:.2f}, {track.velocity[2]:.2f}"
 
+    log_best_track = LogInfoMessage(cooldown=1.0)
+    log_angle_to_cone = LogInfoMessage(cooldown=1.0)
+
+    def get_area(o):
+        top_left = o.bounding_box_2d.corners[0]
+        bottom_right = o.bounding_box_2d.corners[2]
+        area = (bottom_right.kp[0] - top_left.kp[0]) * (bottom_right.kp[1] - top_left.kp[1])
+        return area
+
+    def get_color_confidence(o):
+        red = torch.tensor(o.hist_red)
+        green = torch.tensor(o.hist_green)
+        blue = torch.tensor(o.hist_blue)
+        red = red / red.sum()
+        green = green / green.sum()
+        blue = blue / blue.sum()
+        histo = torch.cat([red, green, blue]).unsqueeze(0)
+        color_confidence = net(histo)
+        return color_confidence.item()
 
     def onObjectDetect(objectsStamped):
-        global best_track
 
+        best_track = None
+        start_time = time.time()
+        largest_area = None
         for o in objectsStamped.objects:
-            rospy.loginfo(format_track(o))
-            # filter condition
-            if o.confidence > 50. and o.tracking_state == 1 and not math.isnan(o.position[0]):
+            # rospy.loginfo(format_track(o))
+            # filter conditions
+            area = get_area(o)
+            color_confidence = get_color_confidence(o)
+            if o.confidence > 50. and o.tracking_state == 0 \
+                and not math.isnan(o.position[0]) \
+                and area > rospy.get_param('/MIN_OBJECT_AREA') \
+                and color_confidence > rospy.get_param('/COLOR_CONFIDENCE'):
+
                 # take track with highest confidence
                 if best_track is None:
                     best_track = o
-                elif o.confidence > best_track.confidence:
+                    largest_area = area
+                elif area > largest_area:
                     best_track = o
+
+            if best_track is not None:
+                log_best_track(f'color_confidence: {color_confidence} confidence: {o.confidence} area: {area}')
+
+            
 
         """
         best_track.position[0] -> distance to object (meters), [1] -> distance left/right (meters), [2] up/down (meters)
         """
         if best_track is not None:
-            # rospy.loginfo(format_track(best_track))
+
             a_to_obj = math.atan(best_track.position[1] / (best_track.position[0] + 0.1) )
-            rospy.loginfo(a_to_obj)
+            log_angle_to_cone(f'angle_to_cone {a_to_obj}')
             angle_to_object.appendleft(a_to_obj)
+
             P = angle_to_object[0]
             I = sum(angle_to_object)
             D = angle_to_object[1] - angle_to_object[0] if len(angle_to_object) > 1 else 0.
@@ -388,78 +464,96 @@ if __name__ == "__main__":
             pos = P * rospy.get_param('/SERVO_P') \
                   + max(rospy.get_param('/SERVO_IMAX'), I * rospy.get_param('/SERVO_I')) \
                   + D * rospy.get_param('/SERVO_D')
-            ctrl.set_servo(get_servo_pwm(pos))
+            #ctrl.set_servo(get_servo_pwm(pos))
+            ctrl.servo = get_servo_pwm(pos)
+
+            dist_to_object.appendleft(best_track.position[0] - 0.2)
+
+            P = dist_to_object[0]
+            I = sum(dist_to_object)
+            D = dist_to_object[1] - dist_to_object[0] if len(dist_to_object) > 1 else 0.
+
+            t_pos = P * rospy.get_param('/THROTTLE_P') \
+                  + max(rospy.get_param('/THROTTLE_IMAX'), I * rospy.get_param('/THROTTLE_I')) \
+                  + D * rospy.get_param('/THROTTLE_D')
+
+            ctrl.throttle = get_throttle_pwm(t_pos)
+            ctrl.set_throttle_servo(get_throttle_pwm(pos), get_servo_pwm(pos))
+
             rate.sleep()
+
 
         best_track = None
 
-zed_obj_det = rospy.Subscriber('/zed2i/zed_node/obj_det/objects', ObjectsStamped, callback=onObjectDetect)
 
-# init zed camera and detector thread
-parser = argparse.ArgumentParser()
-rospy.loginfo(f'offboard arggs: {sys.argv}')
-args = parser.parse_args(sys.argv[4:])
+    zed_obj_det = rospy.Subscriber('/zed2i/zed_node/obj_det/objects', ObjectsStamped, callback=onObjectDetect, queue_size=1)
 
-# Setpoint publishing MUST be faster than 2Hz
-rate = rospy.Rate(20)
-i = 0
+    # init zed camera and detector thread
+    parser = argparse.ArgumentParser()
+    rospy.loginfo(f'offboard arggs: {sys.argv}')
+    args = parser.parse_args(sys.argv[4:])
 
-# Wait for Flight Controller connection
-while (not rospy.is_shutdown() and not current_state.connected):
-    print(current_state.connected)
-    rate.sleep()
+    # Setpoint publishing MUST be faster than 2Hz
+    rate = rospy.Rate(200)
+    i = 0
 
-pose = PoseStamped()
+    # Wait for Flight Controller connection
+    while (not rospy.is_shutdown() and not current_state.connected):
+        print(current_state.connected)
+        rate.sleep()
 
-pose.pose.position.x = 0
-pose.pose.position.y = 0
-pose.pose.position.z = 2
+    pose = PoseStamped()
 
-rospy.loginfo("sending dummy setpoints")
+    pose.pose.position.x = 0
+    pose.pose.position.y = 0
+    pose.pose.position.z = 2
 
-# Send a few setpoints before starting
-for i in range(100):
-    if (rospy.is_shutdown()):
-        break
+    rospy.loginfo("sending dummy setpoints")
 
-    local_pos_pub.publish(pose)
-    rate.sleep()
+    # Send a few setpoints before starting
+    for i in range(100):
+        if (rospy.is_shutdown()):
+            break
 
-offb_set_mode = SetModeRequest()
-offb_set_mode.custom_mode = 'MANUAL'
+        local_pos_pub.publish(pose)
+        rate.sleep()
 
-arm_cmd = CommandBoolRequest()
-arm_cmd.value = True
+    offb_set_mode = SetModeRequest()
+    offb_set_mode.custom_mode = 'MANUAL'
 
-last_req = rospy.Time.now()
+    arm_cmd = CommandBoolRequest()
+    arm_cmd.value = True
 
-while (not rospy.is_shutdown()):
+    last_req = rospy.Time.now()
+    last_log_mesg = rospy.Time.now() - rospy.Duration(30.0)
+    log_armed_message = LogInfoMessage(cooldown=30.0)
 
-    try:
-        if (current_state.mode != "MANUAL" and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
-            rospy.loginfo('setting mode')
-            rospy.loginfo(f'mode: {current_state.mode}')
-            rospy.loginfo(f'armed: {current_state.armed}')
-            if (set_mode_client.call(offb_set_mode).mode_sent == True):
-                rospy.loginfo("MANUAL enabled")
+    while (not rospy.is_shutdown()):
 
-            last_req = rospy.Time.now()
-        else:
+        try:
+            if (current_state.mode != "MANUAL" and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
+                rospy.loginfo('setting mode')
+                rospy.loginfo(f'mode: {current_state.mode}')
+                rospy.loginfo(f'armed: {current_state.armed}')
+                if (set_mode_client.call(offb_set_mode).mode_sent == True):
+                    rospy.loginfo("MANUAL enabled")
 
-            # if (not current_state.armed and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
-            #     rospy.loginfo('arming')
-            #     rospy.loginfo(f'mode: {current_state.mode}')
-            #     rospy.loginfo(f'armed: {current_state.armed}')
-            #     if (arming_client.call(arm_cmd).success == True):
-            #         rospy.loginfo("Vehicle armed")
-            #         rospy.loginfo('Vehicle armed')
+                last_req = rospy.Time.now()
+            else:
 
-            last_req = rospy.Time.now()
+                if (not current_state.armed and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
+                    rospy.loginfo('arming')
+                    rospy.loginfo(f'mode: {current_state.mode}')
+                    rospy.loginfo(f'armed: {current_state.armed}')
+                if (arming_client.call(arm_cmd).success == True):
+                    log_armed_message("Vehicle armed")
+
+                last_req = rospy.Time.now()
         # print(pose)
         # local_pos_pub.publish(pose)
 
-        i += 1
-        rate.sleep()
-    except Exception as e:
-        rospy.logerr(e)
-        rospy.logerr(traceback.format_exc())
+                i += 1
+                rate.sleep()
+        except Exception as e:
+            rospy.logerr(e)
+            rospy.logerr(traceback.format_exc())
